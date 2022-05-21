e510c9cd7b46b2c292dc14540063438bed19248f
BruceChen7 <bofengqiye7@gmail.com>
2022-05-21 14:39:20 +0800

    feat(wezterm): when use wayland, then enable wayland

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,19 @@
 local wezterm = require("wezterm")
+local function enable_wayland()
+	wayland = os.getenv("XDG_SESSION_TYPE")
+	if wayland ~= "wayland" then
+		return false
+	end
+	return true
+end
 return {
 	enable_tab_bar = false,
 	font_size = 15,
 	use_ime = true,
 	font = wezterm.font("JetBrainsMono Nerd Font"),
+	enable_wayland = enable_wayland(),
+	keys = {
+		{ key = "c", mods = "CTRL", action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }) },
+		{ key = " ", mods = "ALT", action = "Nop" },
+	},
 }
