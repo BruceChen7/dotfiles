8208bfe24822538cb37c2a919860f8be76b31317
BruceChen7 <bofengqiye7@gmail.com>
2022-08-21 16:59:27 +0800

    feat(neovim): use new diffview.nvim config

--- .config/nvim/lua/config/diff.lua
+++ .config/nvim/lua/config/diff.lua
@@ -3,9 +3,18 @@ local actions = require("diffview.config").actions
 
 diffview.setup {
   diff_binaries = false,
+  use_icons = true,
+  icons = { -- Only applies when use_icons is true.
+    folder_closed = "",
+    folder_open = "",
+  },
+  signs = {
+    fold_closed = "",
+    fold_open = "",
+  },
   hooks = {
     diff_buf_read = function(bufnr)
-      vim.api.nvim_buf_del_keymap(bufnr, "n", "<tab>")
+      -- vim.api.nvim_buf_del_keymap(bufnr, "n", "<tab>")
       local opts = { noremap = true, silent = true }
       vim.api.nvim_buf_set_keymap(bufnr, "n", "<tab>l", "<c-w>l", opts)
       vim.api.nvim_buf_set_keymap(bufnr, "n", "<tab>j", "<c-w>j", opts)
@@ -16,7 +25,7 @@ diffview.setup {
       -- dump(view)
       local opts = { noremap = true, silent = true }
       bufnr = view.panel.bufid
-      vim.api.nvim_buf_del_keymap(bufnr, "n", "<tab>", opts)
+      -- vim.api.nvim_buf_del_keymap(bufnr, "n", "<tab>", opts)
       local opts = { noremap = true }
       vim.api.nvim_buf_set_keymap(bufnr, "n", "<tab>l", "<c-w>l", opts)
       vim.api.nvim_buf_set_keymap(bufnr, "n", "<tab>j", "<c-w>j", opts)
@@ -24,50 +33,52 @@ diffview.setup {
       vim.api.nvim_buf_set_keymap(bufnr, "n", "<tab>h", "<c-w>h", opts)
     end,
   },
-  key_bindings = {
+  keymaps = {
     disable_defaults = false,
-
     view = {
-      ["n"] = actions.select_next_entry, -- Open the diff for the next file
-      ["p"] = actions.select_prev_entry, -- Open the diff for the previous file
-      -- ["<tab>l"] = actions "focus_entry",
       ["<tab>"] = false,
       ["<s-tab>"] = false,
-      -- ["gf"] = actions "goto_file", -- Open the file in a new split in previous tabpage
-      ["gf"] = actions.goto_file_split, -- Open the file in a new split
-      ["gft"] = actions.goto_file_tab, -- Open the file in a new tabpage
-      ["<leader>e"] = actions.focus_files, -- Bring focus to the files panel
-      ["<leader>b"] = actions.toggle_files, -- Toggle the files panel.
-    },
-    file_panel = {
-      ["n"] = actions.select_next_entry,
-      -- ["<tab>l"] = actions "focus_entry",
-      ["p"] = actions.select_prev_entry,
-      ["<cr>"] = actions.select_entry,
-      ["<up>"] = actions.prev_entry,
-      ["<ctrl-j>"] = actions.prev_entry,
-      ["<ctrl-k>"] = actions.select_entry, -- Open the diff for the selected entry.
-      ["o"] = actions.select_entry,
-      ["-"] = actions.toggle_stage_entry, -- Stage / unstage the selected entry.
-      ["S"] = actions.stage_all, -- Stage all entries.
-      ["U"] = actions.unstage_all, -- Unstage all entries.
-      ["X"] = actions.restore_entry, -- Restore entry to the state on the left side.
-      ["R"] = actions.refresh_files, -- Update stats and entries in the file list.
       ["gf"] = actions.goto_file,
-      ["gss"] = actions.goto_file_split,
-      ["gst"] = actions.goto_file_tab,
-      ["i"] = actions.listing_style, -- Toggle between 'list' and 'tree' views
-      ["f"] = actions.toggle_flatten_dirs, -- Flatten empty subdirectories in tree listing style.
+      ["<C-w><C-f>"] = actions.goto_file_split,
+      ["<C-w>gf"] = actions.goto_file_tab,
       ["<leader>e"] = actions.focus_files,
       ["<leader>b"] = actions.toggle_files,
+    },
+    file_panel = {
+      ["j"] = actions.select_next_entry,
+      ["<down>"] = actions.next_entry,
+      ["k"] = actions.select_prev_entry,
+      ["<up>"] = actions.prev_entry,
+      ["<cr>"] = actions.select_entry,
+      ["o"] = actions.select_entry,
+      ["<2-LeftMouse>"] = actions.select_entry,
+      ["-"] = actions.toggle_stage_entry,
+      ["S"] = actions.stage_all,
+      ["U"] = actions.unstage_all,
+      ["X"] = actions.restore_entry,
+      ["R"] = actions.refresh_files,
+      ["L"] = actions.open_commit_log,
+      ["<c-b>"] = actions.scroll_view(-0.25),
+      ["<c-f>"] = actions.scroll_view(0.25),
+      -- ["<tab>"] = actions.select_next_entry,
+      -- ["<s-tab>"] = actions.select_prev_entry,
       ["<tab>"] = false,
       ["<s-tab>"] = false,
-      ["q"] = actions.close,
+      ["<tab>"] = nil,
+      ["<s-tab>"] = nil,
+      ["gf"] = actions.goto_file,
+      ["<C-w><C-f>"] = actions.goto_file_split,
+      ["<C-w>gf"] = actions.goto_file_tab,
+      ["i"] = actions.listing_style,
+      ["f"] = actions.toggle_flatten_dirs,
+      ["<leader>e"] = actions.focus_files,
+      ["<leader>b"] = actions.toggle_files,
     },
     file_history_panel = {
-      ["g!"] = actions.options, -- Open the option panel
-      ["<C-A-d>"] = actions.open_in_diffview, -- Open the entry under the cursor in a diffview
-      ["y"] = actions.copy_hash, -- Copy the commit hash of the entry under the cursor
+      ["g!"] = actions.options,
+      ["<C-A-d>"] = actions.open_in_diffview,
+      ["y"] = actions.copy_hash,
+      ["L"] = actions.open_commit_log,
       ["zR"] = actions.open_all_folds,
       ["zM"] = actions.close_all_folds,
       ["j"] = actions.next_entry,
@@ -77,8 +88,12 @@ diffview.setup {
       ["<cr>"] = actions.select_entry,
       ["o"] = actions.select_entry,
       ["<2-LeftMouse>"] = actions.select_entry,
-      ["n"] = actions.select_next_entry,
-      ["p"] = actions.select_prev_entry,
+      ["<c-b>"] = actions.scroll_view(-0.25),
+      ["<c-f>"] = actions.scroll_view(0.25),
+      -- ["<tab>"] = actions.select_next_entry,
+      -- ["<s-tab>"] = actions.select_prev_entry,
+      ["<tab>"] = false,
+      ["<s-tab>"] = false,
       ["gf"] = actions.goto_file,
       ["<C-w><C-f>"] = actions.goto_file_split,
       ["<C-w>gf"] = actions.goto_file_tab,
@@ -86,10 +101,9 @@ diffview.setup {
       ["<leader>b"] = actions.toggle_files,
     },
     option_panel = {
-      ["n"] = actions.select_entry,
-      ["q"] = actions.close,
+      -- ["<tab>"] = actions.select_entry,
       ["<tab>"] = false,
-      ["<s-tab>"] = false,
+      ["q"] = actions.close,
     },
   },
 }
