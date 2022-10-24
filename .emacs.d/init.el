932d134ea7d84e00a4b1d11d335a109aad843645
BruceChen7 <bofengqiye7@gmail.com>
2022-10-25 02:29:02 +0800

    feat(emacs): make emacs more useful

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,5 @@
 ;;(setq inhibit-startup-message t)             ; 关闭启动 Emacs 时的欢迎界面
+(setq visible-bell nil)
 (setq gc-cons-threshold (* 100 1024 1024))
 (electric-pair-mode t)                       ; 自动补全括号
 (add-hook 'prog-mode-hook #'show-paren-mode) ; 编程模式下，光标在括号上时高亮另一个括号
@@ -14,45 +15,65 @@
 (when (display-graphic-p) (toggle-scroll-bar -1)) ; 图形界面时关闭滚动条
 
 (savehist-mode 0)                            ; （可选）打开 Buffer 历史记录保存
-(setq display-line-numbers-type 'relative)   ; （可选）显示相对行号
-(setq inhibit-startup-screen t
-      initial-buffer-choice  nil)
 
-(setq inhibit-splash-screen t)
+;; make :q just kill the buffer
+(global-set-key [remap evil-quit] 'kill-buffer-and-window)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(fset 'yes-or-no-p 'y-or-n-p)
 
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+(setq initial-major-mode (quote fundamental-mode))
 
 (require 'package)
 (setq package-archives '(("gnu"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")
                          ("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")))
 (package-initialize)
 
-;; (use-package awesome-tab
-;;   :load-path "~/.emacs.d/elpa/awesome-tab"
-;;   :config
-;;   (awesome-tab-mode t))
 
 
 (eval-when-compile
   (require 'use-package))
 
 
+;;(use-package dashboard
+;;  :ensure t
+;;  :config
+;;  (dashboard-setup-startup-hook))
+
 (use-package counsel
   :ensure t)
 
+
+(use-package highlight-indent-guides
+  :ensure t
+  :config
+    (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
+
+
 (use-package all-the-icons
   :ensure t)
+
 
 (use-package doom-modeline
   :ensure t
   :config
   (setq doom-modeline-support-imenu t) 
   :init (doom-modeline-mode 1))
+
 (use-package ivy
   :diminish
   :ensure t            
   :bind(("C-s" . swiper)
+	("C-c r" . counsel-recentf)
 	:map ivy-minibuffer-map
 	("TAB" . ivy-alt-done)
 	("C-l" . ivy-alt-done)
@@ -69,11 +90,6 @@
 (load-theme 'atom-one-dark t)
 
 
-(use-package projectile
-  :ensure t
-  :config
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1))
 
 (use-package company
   :hook ((prog-mode . company-mode)
@@ -87,6 +103,15 @@
  :config
  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-window-center)))
  (ivy-posframe-mode 1))
+
+(use-package projectile
+  :ensure t
+  :config
+  (setq projectile-sort-order 'recently-active)
+  (setq projectile-enable-caching t)
+  (setq projectile-completion-system 'ivy)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
 
 ;; used to move cursor fastly
 (use-package avy
@@ -132,6 +157,8 @@
     lsp-ui-peek-enable t
     lsp-ui-sideline-enable t
     lsp-ui-imenu-enable t
+    lsp-ui-sideline-show-hover nil
+    lsp-ui-doc-position 'at-point
     lsp-ui-flycheck-enable t)
 
 ;; LSP
@@ -164,6 +191,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight-indent-guides-method 'bitmap)
  '(package-selected-packages
    '(company zzz-to-char lsp-ui go-mode use-package lsp-mode counsel)))
 (custom-set-faces
