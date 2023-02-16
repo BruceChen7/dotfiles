41ec62ff916ce53b4a61a181a7a2cb7578e884c8
BruceChen7 <bofengqiye7@gmail.com>
2023-02-16 11:22:12 +0800

    feat: remove packer related code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,8 @@
 -- for speed up nvim load
-do
-  local ok, _ = pcall(require, "impatient")
-  if not ok then
-    print "could not call impatient"
-  end
-end
-
 require "util"
-require "util.packer"
+require "plugins"
 require "buildin"
 require "options"
-require "plugins"
 require "keymaps"
 require "style"
 require "lsp"
