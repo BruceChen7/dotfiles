f220004b8cf887885704f5df1edfa531729920c9
BruceChen7 <bofengqiye7@gmail.com>
2023-04-12 18:08:50 +0800

    Improve keyboard mappings and on_attach function in LSP configuration
    - Map new keyboard shortcuts for diagnostic information
    - Use `nvim_create_autocmd` to delay key mapping
    - Update completion triggering and disable formatting in `on_attach`
    - Improve key mapping syntax
    - Remove redundant macOS key mapping call

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -18,31 +18,44 @@ local signature_config = {
 
 require("lsp_signature").setup(signature_config)
 
-function set_key()
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  u.map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  u.map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  u.map("n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  u.map("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  u.map("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  u.map("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  u.map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  u.map("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  u.map("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  u.map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  u.map("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  u.map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-  u.map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-  u.map("n", "<space>d", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  u.map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  -- u.map("n", "gpd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  -- u.map("n", "gsd", "<cmd> vsplit | lua vim.lsp.buf.definition()<CR>", opts)
-  u.map("n", "gd", "<cmd> lua jump_to_definition()<CR>", opts)
-  u.map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-end
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+    -- u.map("n", "gpd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    -- u.map("n", "gsd", "<cmd> vsplit | lua vim.lsp.buf.definition()<CR>", opts)
+    vim.keymap.set("n", "gd", "<cmd> lua jump_to_definition()<CR>", opts)
+  end,
+})
 
 function jump_to_definition()
   local params = vim.lsp.util.make_position_params()
@@ -121,14 +134,6 @@ local on_attach = function(client, bufnr)
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-  if vim.loop.os_uname().sysname == "Linux" then
-    set_key()
-  end
-end
-
--- if not set, macos not working
-if vim.loop.os_uname().sysname ~= "Linux" then
-  set_key()
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
