a61ee5b21740f4974dc981d4b881624e958c50ed
BruceChen7 <bofengqiye7@gmail.com>
2023-07-23 12:56:53 +0800

    feat: reset cmp config

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -59,7 +59,7 @@ cmp.setup {
     ),
   },
   experimental = {
-    ghost_text = true,
+    ghost_text = false,
     native_menu = false,
   },
   sources = {
@@ -80,31 +80,20 @@ cmp.setup {
     { name = "crates" },
     { name = "cmp_tabnine", priority = 700 },
   },
+  matching = {
+    disallow_fuzzy_matching = true,
+    disallow_fullfuzzy_matching = true,
+    disallow_partial_fuzzy_matching = false,
+    disallow_partial_matching = false,
+    disallow_prefix_unmatching = true,
+  },
   sorting = {
-    -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
     comparators = {
+      cmp.config.compare.locality,
       cmp.config.compare.offset,
       cmp.config.compare.exact,
+      cmp.config.compare.recently_used,
       cmp.config.compare.score,
-
-      -- copied from cmp-under, but I don't think I need the plugin for this.
-      -- I might add some more of my own.
-      function(entry1, entry2)
-        local _, entry1_under = entry1.completion_item.label:find "^_+"
-        local _, entry2_under = entry2.completion_item.label:find "^_+"
-        entry1_under = entry1_under or 0
-        entry2_under = entry2_under or 0
-        if entry1_under > entry2_under then
-          return false
-        elseif entry1_under < entry2_under then
-          return true
-        end
-      end,
-
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
     },
   },
 
@@ -164,28 +153,28 @@ local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflictsclient
-format_group = vim.api.nvim_create_augroup("FormatGroup", { clear = true })
-format_file_type = { "go", "zig", "md", "rs", "lua", "py" }
-for _, v in ipairs(format_file_type) do
-  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = string.format("*.%s", v),
-    group = format_group,
-    callback = function()
-      vim.lsp.buf.format()
-    end,
-  })
-end
-
-vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = "LspAttach_inlayhints",
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
-
-    local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    require("lsp-inlayhints").on_attach(client, bufnr)
-  end,
-})
+-- format_group = vim.api.nvim_create_augroup("FormatGroup", { clear = true })
+-- format_file_type = { "go", "zig", "md", "rs", "lua", "py" }
+-- for _, v in ipairs(format_file_type) do
+--   vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--     pattern = string.format("*.%s", v),
+--     group = format_group,
+--     callback = function()
+--       vim.lsp.buf.format()
+--     end,
+--   })
+-- end
+--
+-- vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   group = "LspAttach_inlayhints",
+--   callback = function(args)
+--     if not (args.data and args.data.client_id) then
+--       return
+--     end
+--
+--     local bufnr = args.buf
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     require("lsp-inlayhints").on_attach(client, bufnr)
+--   end,
+-- })
