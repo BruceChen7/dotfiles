c84ded7ce645ddbe001cff7aa15deb27912e4d76
BruceChen7 <bofengqiye7@gmail.com>
2023-08-24 00:05:27 +0800

    feat: more config

--- .config/nvim/lua/config/text_obj.lua
+++ .config/nvim/lua/config/text_obj.lua
@@ -25,7 +25,7 @@ tc.setup {
     -- end,
   },
   incremental_selection = {
-    enable = true,
+    enable = false,
     keymaps = {
       init_selection = "<CR>",
       node_incremental = "<CR>",
