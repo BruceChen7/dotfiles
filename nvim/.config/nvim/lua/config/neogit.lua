072661b84462a9465f4076031a86a82f794dbd29
BruceChen7 <bofengqiye7@gmail.com>
2023-08-29 15:31:51 +0800

    feat: more config

--- nvim/.config/nvim/lua/config/neogit.lua
+++ nvim/.config/nvim/lua/config/neogit.lua
@@ -8,13 +8,9 @@ function open_neogit()
     cwd = vim.fn.getcwd()
   end
   print("open " .. cwd .. " repository")
-  neogit.open { kind = "split_above", cwd = cwd }
+  neogit.open {}
 end
 vim.keymap.set("n", "<space>gg", ":lua open_neogit()<CR>", { silent = true })
-
-vim.cmd [[
-    autocmd CursorMoved NeogitCommitView lua not_use_tab_key_map()
-]]
 
 neogit.setup {
   disable_signs = false,
@@ -38,63 +34,12 @@ neogit.setup {
   integrations = {
     diffview = true,
   },
-  -- Setting any section to `false` will make the section not render at all
-  sections = {
-    untracked = {
-      folded = false,
-    },
-    unstaged = {
-      folded = false,
-    },
-    staged = {
-      folded = false,
-    },
-    stashes = {
-      folded = true,
-    },
-    unpulled = {
-      folded = true,
-    },
-    unmerged = {
-      folded = false,
-    },
-    recent = {
-      folded = true,
-    },
-  },
-  -- override/add mappings
   mappings = {
-    -- modify status buffer mappings
+    -- status = default_mapping_values.mappings.status
     status = {
-      -- Adds a mapping with "B" as key that does the "BranchPopup" command
-      -- ["B"] = "BranchPopup",
+      ["<tab>"] = nil,
       ["="] = "Toggle",
-      -- use tab h to switch buffers
-      ["<tab>"] = "",
     },
   },
 }
 
-set_tabkey_for_neogit = 0
-function not_use_tab_key_map()
-  if set_tabkey_for_neogit == 1 then
-    return
-  end
-  status = require "neogit.status"
-  if status == nil or status.commit_view == nil or not status.commit_view.buffer then
-    return
-  end
-  local commit_view = status.commit_view
-  -- dump(status.commit_view)
-  local mappings = commit_view.buffer.mmanager.mappings["<tab>"]
-  if mappings == nil then
-    commit_view.buffer.mmanager.register()
-    return
-  end
-  local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, "n", "<tab>", "<Nop>", opts)
-  commit_view.buffer.mmanager.mappings["="] = mappings
-  commit_view.buffer.mmanager.mappings["<tab>"] = nil
-  commit_view.buffer.mmanager.register()
-  set_tabkey_for_neogit = 1
-end
