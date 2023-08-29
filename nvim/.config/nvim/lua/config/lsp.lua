48a1bf6917a1df90c76de4bb5b59ad3060a3baf8
BruceChen7 <bofengqiye7@gmail.com>
2023-08-29 16:34:11 +0800

    feat: more config

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -236,3 +236,10 @@ require("lsp-setup").setup {
 --     vim.lsp.buf.code_action { context = { only = { "source.organizeImports" } }, apply = true }
 --   end,
 -- })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.md",
+  callback = function()
+    MiniTrailspace.trim()
+  end,
+})
