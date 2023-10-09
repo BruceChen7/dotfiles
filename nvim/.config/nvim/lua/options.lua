2347f72f206c7929aee7f7887130d96d31f3c700
BruceChen7 <bofengqiye7@gmail.com>
2023-10-09 17:48:06 +0800

    feat(nvim): Enable auto-refresh of buffers
    - Set autoread option to true
    - Create autocmd for BufEnter, CursorHold, CursorHoldI, and FocusGained events
    - Add checktime command to refresh buffers on event

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -125,3 +125,10 @@ vim.cmd [[
  set clipboard+=unnamedplus
  set foldcolumn=0
 ]]
+
+-- https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482/3
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
