25da350d5f18faf6b1933d73279e3491997b4b15
BruceChen7 <bofengqiye7@gmail.com>
2023-11-29 19:03:34 +0800

    feat: more plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -786,37 +786,15 @@ require("lazy").setup {
   },
 
   {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
+    "jakewvincent/mkdnflow.nvim",
+    config = function()
+      require("mkdnflow").setup {
+        -- Config goes here; leave blank for defaults
+      }
+      -- current buffer only
+      vim.keymap.set("n", "<leader>mk", ":MkdnCreateLink<CR>", { silent = true, desc = "create link" })
+    end,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
-    dependencies = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-
-      -- see below for full list of optional dependencies 👇
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/work/notes",
-        },
-      },
-      daily_notes = {
-        path = "~/work/notes/Calendar/Daily Notes",
-      },
-
-      disable_frontmatter = true,
-      -- see below for full list of options 👇
-    },
   },
 
   {
