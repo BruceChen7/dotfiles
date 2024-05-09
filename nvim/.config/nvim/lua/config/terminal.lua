b79baefa5c8fc6c581ebb695e6585ec90dcbd16b
BruceChen7 <bofengqiye7@gmail.com>
2024-05-10 00:36:39 +0800

    feat: make  work in

--- nvim/.config/nvim/lua/config/terminal.lua
+++ nvim/.config/nvim/lua/config/terminal.lua
@@ -20,16 +20,17 @@ require("toggleterm").setup {
 
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
+  -- using <esc> to enter normal mode
   vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
   -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<tab>h", [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<tab>j", [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<tab>k", [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<tab>l", [[<C-\><C-n><C-W>l]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<tab>h", [[<C-\><C-n><C-W>h]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<tab>j", [[<C-\><C-n><C-W>j]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<tab>k", [[<C-\><C-n><C-W>k]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<tab>l", [[<C-\><C-n><C-W>l]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
+-- vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
 
 local Terminal = require("toggleterm.terminal").Terminal
 
