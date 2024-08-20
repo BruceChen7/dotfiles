cff7564ab37e20a8f37e63ac6ca9c79d27f2c74f
BruceChen7 <bofengqiye7@gmail.com>
2024-08-20 17:24:50 +0800

    feat(plugs): use avante

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -613,6 +613,53 @@ require("lazy").setup {
     event = "VeryLazy",
   },
 
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make BUILD_FROM_SOURCE=true luajit",
+    opts = {
+      -- add any opts here
+      provider = "deepseek",
+      mappings = {
+        ask = "<leader>ak",
+        edit = "<leader>ae",
+        refresh = "<leader>ar",
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          none = "c0",
+          both = "cb",
+          next = "]x",
+          prev = "[x",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+      },
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      {
+        "grapp-dev/nui-components.nvim",
+        dependencies = {
+          "MunifTanjim/nui.nvim",
+        },
+      },
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      -- {
+      --   "MeanderingProgrammer/render-markdown.nvim",
+      --   opts = {
+      --     file_types = { "markdown", "Avante" },
+      --   },
+      --   ft = { "markdown", "Avante" },
+      -- },
+    },
+  },
+
   -- "Preview command results with `:Norm`"
   {
     "smjonas/live-command.nvim",
