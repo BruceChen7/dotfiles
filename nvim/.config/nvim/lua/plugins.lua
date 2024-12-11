0a0c00dc89053cf991815b26a2bd64d2a2a9f65f
bofengqiye <bofengqiye7@gmail.com>
2024-12-11 20:41:41 +0800

    feat: add bigfile.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1063,6 +1063,25 @@ require("lazy").setup {
   --   event = "VeryLazy",
   -- },
   {
+    "LunarVim/bigfile.nvim",
+    config = function()
+      require("bigfile").setup {
+        filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+        pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
+        features = { -- features to disable
+          -- "indent_blankline",
+          -- "illuminate",
+          -- "lsp",
+          "treesitter",
+          "syntax",
+          "matchparen",
+          "vimopts",
+          "filetype",
+        },
+      }
+    end,
+  },
+  {
     "otavioschwanck/arrow.nvim",
     opts = {
       show_icons = true,
