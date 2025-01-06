84783ea64ea21ea5d1d399020b848237288be704
bofengqiye <bofengqiye7@gmail.com>
2025-01-06 13:08:11 +0800

    chore(emacs): more configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,13 +6,14 @@
 (show-paren-mode 1)  ; Highlight matching parentheses
 
 
+(setq make-backup-files nil) ; 禁用备份文件
+(setq auto-save-default nil) ; 禁用自动保存文件
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode 1)
 
 (add-to-list 'default-frame-alist '(font . "Iosevka-14"))
-
-;;; PACKAGE LIST
+;; PACKAGE LIST
 (setq package-install-upgrade-built-in t)
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
@@ -21,10 +22,10 @@
         ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 
 ;;; BOOTSTRAP USE-PACKAGE
-; (package-initialize)
+
 
 ; 确保在编译时加载use-package
-; 启用use-package的详细输出
+; 启用use-pacage的详细输出
 ; 禁用原生编译时的异步错误报告
 (eval-when-compile (require 'use-package))
 (setq use-package-verbose t
@@ -33,7 +34,6 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
-
 
 (require 'meow)
 (defun meow-setup ()
@@ -125,5 +125,117 @@
 (meow-setup)
 (meow-global-mode 1)
 
+; 这段代码通过 `consult` 包增强了 Emacs 的搜索、选择和导航功能，提供了更高效的方式来查找文件、缓冲区、行、标记等，
+; 并集成了多种工具（如 ripgrep、Flymake 等）。
+(use-package consult
+  :ensure t
+  :after vertico
+  :bind (("C-x b"       . consult-buffer)
+         ("C-x C-k C-k" . consult-kmacro)
+         ("M-y"         . consult-yank-pop)
+         ("M-g g"       . consult-goto-line)
+         ("M-g M-g"     . consult-goto-line)
+         ("M-g f"       . consult-flymake)
+         ("M-g i"       . consult-imenu)
+         ("M-s l"       . consult-line)
+         ("M-s L"       . consult-line-multi)
+         ("M-s u"       . consult-focus-lines)
+         ("M-s g"       . consult-ripgrep)
+         ("M-s M-g"     . consult-ripgrep)
+         ("M-s f"       . consult-find)
+         ("M-s M-f"     . consult-find)
+         ("C-x C-SPC"   . consult-global-mark)
+         ("C-x M-:"     . consult-complex-command)
+         ("C-c n"       . consult-org-agenda)
+         ("M-X"         . consult-mode-command)
+         :map minibuffer-local-map
+         ("M-r" . consult-history)
+         :map Info-mode-map
+         ("M-g i" . consult-info)
+         ; :map org-mode-map
+         ("M-g i"  . consult-org-heading))
+  :custom
+  (completion-in-region-function #'consult-completion-in-region)
+  :config
+  (recentf-mode t))
+
+; mini buffer的补全框架
+; 提供现代化的补全界面
+; 支持模糊匹配和灵活补全
+; 在补全时显示额外信息
+; 改善minibuffer的使用体验
+(use-package vertico
+  :ensure t
+  :init
+  ;; Enable vertico using the vertico-flat-mode
+  (require 'vertico-directory)
+  (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)
+
+  (use-package orderless
+    :ensure t
+    :commands (orderless)
+    :custom (completion-styles '(orderless flex)))
+
+  (use-package marginalia
+    :ensure t
+    :custom
+    (marginalia-annotators
+     '(marginalia-annotators-heavy marginalia-annotators-light nil))
+    :config
+    (marginalia-mode))
+  (vertico-mode t)
+  :bind(:map vertico-map
+              ("C-j" . vertico-next)     ; 下一个候选项
+              ("C-k" . vertico-previous)) ; 上一个候选项
+  :config
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t)
+  (minibuffer-depth-indicate-mode 1))
+
+(use-package embark
+  :ensure t
+  :bind
+  ;; pick some comfortable binding
+  (("C-="                     . embark-act)
+   ([remap describe-bindings] . embark-bindings)
+   :map embark-file-map
+   ("C-d" . dragon-drop)
+   :map embark-defun-map
+   ("M-t" . chatgpt-gen-tests-for-region)
+   :map embark-general-map
+   ("M-c" . chatgpt-prompt)
+   :map embark-region-map
+   ("?"   . chatgpt-explain-region)
+   ("M-f" . chatgpt-fix-region)
+   ("M-f" . chatgpt-fix-region))
+  :custom
+  (embark-indicators
+   '(embark-highlight-indicator
+     embark-isearch-highlight-indicator
+     embark-minimal-indicator))
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  (setq embark-prompter 'embark-completing-read-prompter)
+  :config
+  (defun dragon-drop (file)
+    (start-process-shell-command "dragon-drop" nil
+                                 (concat "dragon-drop " file)))
+
+  ;; Preview any command with M-.
+  (keymap-set minibuffer-local-map "M-." 'my-embark-preview)
+  (defun my-embark-preview ()
+    "Previews candidate in vertico buffer, unless it's a consult command"
+    (interactive)
+    (unless (bound-and-true-p consult--preview-function)
+      (save-selected-window
+        (let ((embark-quit-after-action nil))
+          (embark-dwim))))))
+
 
 (package-initialize)
+
