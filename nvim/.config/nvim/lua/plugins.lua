1b7b05d8a0010915d848f3a4741cf90573ce2083
bofengqiye <bofengqiye7@gmail.com>
2025-03-30 12:29:23 +0800

    chore(tmux): add more confs to swap window chore(nvim): replace aider plugs with other plugs

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1429,29 +1429,29 @@ require("lazy").setup {
   },
 
   {
-    "GeorgesAlkhouri/nvim-aider",
-    cmd = {
-      "AiderTerminalToggle",
-      "AiderHealth",
-    },
-    keys = {
-      { "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
-      { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
-      { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
-      { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-      { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-      { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
-      { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-      -- Example nvim-tree.lua integration if needed
-      { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
-      { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
-    },
-    dependencies = {
-      "folke/snacks.nvim",
-    },
+    "joshuavial/aider.nvim",
     config = function()
-      require "config/aider"
+      require("aider").setup {
+        auto_manage_context = false,
+        default_bindings = false,
+        debug = true,
+        vim = true,
+        ignore_buffers = {},
+
+        -- only necessary if you want to change the default keybindings. <Leader>C is not a particularly good choice. It's just shown as an example.
+        vim.api.nvim_set_keymap(
+          "n",
+          "<leader>aa",
+          ":AiderOpen --no-auto-commits --stream --watch-files --subtree-only<CR>",
+          { noremap = true, silent = true, desc = "Aider Open" }
+        ),
+        vim.api.nvim_set_keymap(
+          "n",
+          "<leader>am",
+          ":AiderAddModifiedFiles<CR>",
+          { noremap = true, silent = true, desc = "Aider Add Modified Files" }
+        ),
+      }
     end,
-    event = "VeryLazy",
   },
 }
