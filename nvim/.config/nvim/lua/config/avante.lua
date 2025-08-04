3d52225ce9ab08f2957281b990547f878baa72c0
bofengqiye <bofengqiye7@gmail.com>
2025-08-04 12:26:33 +0800

    chore(nvim): remove unused llm providers

--- nvim/.config/nvim/lua/config/avante.lua
+++ nvim/.config/nvim/lua/config/avante.lua
@@ -7,12 +7,7 @@ require("avante").setup {
   mode = "agentic",
   -- provider = "chatgpt",
   provider = "freedeepseek",
-  -- provider = "gemini",
-  -- provider = "openrouter",
-  -- cursor_applying_provider = "groq",
   memory_summary_provider = "freedeepseek",
-  -- cursor_applying_provider = "week_operouter",
-  -- auto_suggestions_provider = "deepseek",
   mappings = {
     ask = "\\ak",
     edit = "\\am",
@@ -77,28 +72,6 @@ require("avante").setup {
       api_key_name = "GEMINI_API_KEY",
     },
 
-    openrouter = {
-      __inherited_from = "openai",
-      endpoint = "https://openrouter.ai/api/v1",
-      api_key_name = "OPENROUTER_API_KEY",
-      model = "deepseek/deepseek-r1:free",
-      -- model = "deepseek/deepseek-chat-v3-0324:free",
-      disable_tools = true,
-      extra_request_body = {
-        temperature = 0,
-      },
-    },
-
-    week_operouter = {
-      __inherited_from = "openai",
-      endpoint = "https://openrouter.ai/api/v1",
-      api_key_name = "OPENROUTER_API_KEY",
-      -- model = "google/gemini-2.5-pro-exp-03-25:free",
-      -- model = "deepseek/deepseek-r1:free",
-      model = "meta-llama/llama-3.3-70b-instruct:free",
-      disable_tools = true,
-      -- temperature = 0,
-    },
 
     deepseek = {
       __inherited_from = "openai",
@@ -109,22 +82,6 @@ require("avante").setup {
       extra_request_body = {
         temperature = 0,
       },
-    },
-    groq = { -- define groq provider
-      __inherited_from = "openai",
-      -- api_key_name = "GROQ_API_KEY",
-      api_key_name = "FREE_DEEPSEEK_API_KEY",
-      endpoint = "https://api.groq.com/openai/v1/",
-      model = "llama3.3-70b",
-      max_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-    },
-    real_groq = { -- define groq provider
-      __inherited_from = "openai",
-      api_key_name = "GROQ_API_KEY",
-      endpoint = "https://api-proxy.me/groq/v1",
-      model = "llama-3.3-70b-versatile",
-      max_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-      disable_tools = true,
     },
     qwen3 = {
       __inherited_from = "openai",
@@ -143,9 +100,6 @@ require("avante").setup {
       __inherited_from = "openai",
       api_key_name = "FREE_DEEPSEEK_API_KEY",
       endpoint = "https://ai.nahcrof.com/v2",
-      -- model = "deepseek-v3-0324",
-      -- model = "deepseek-v3-0324",
-      -- model = "deepseek-r1-0528",
       model = "glm-4.5",
       -- https://ai.nahcrof.com/pricing
       max_tokens = 134000,
@@ -167,20 +121,6 @@ require("avante").setup {
       disable_tools = true,
       extra_request_body = {
         temperature = 0.7,
-      },
-    },
-    -- https://linux.do/t/topic/783000
-    chatgpt = {
-      __inherited_from = "openai",
-      api_key_name = "FREE_DEEPSEEK_API_KEY",
-      endpoint = "https://api-0711-node144.be-a.dev/api/layers/openai/bulk",
-      model = "gpt-4.1",
-      -- https://ai.nahcrof.com/pricing
-      -- max_tokens = 134000,
-      -- max_tokens = 134000,
-      disable_tools = true,
-      extra_request_body = {
-        temperature = 0,
       },
     },
   },
