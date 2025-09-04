050892ae4617f8b99d64e67f3d84ae8505b3f5fc
bofengqiye <bofengqiye7@gmail.com>
2025-09-04 19:06:17 +0800

    chore: remove commented here.term plugin configuration

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -213,39 +213,6 @@ require("lazy").setup {
     },
   },
 
-  -- {
-  --   "jaimecgomezz/here.term",
-  --   config = function()
-  --     require("here-term").setup {
-  --       -- The command we run when exiting the terminal and no other buffers are listed. An empty
-  --       -- buffer is shown by default.
-  --       startup_command = "enew", -- Startify, Dashboard, etc. Make sure it has been loaded before `here.term`.
-  --
-  --       -- Mappings
-  --       -- Every mapping bellow can be customized by providing your preferred combo, or disabled
-  --       -- entirely by setting them to `nil`.
-  --       --
-  --       -- The minimal mappings used to toggle and kill the terminal. Available in
-  --       -- `normal` and `terminal` mode.
-  --       mappings = {
-  --         enable = true,
-  --         toggle = "<C-\\>",
-  --         kill = "<C-S-\\>",
-  --       },
-  --       -- Additional mappings that I consider useful since you won't have to escape (<C-\><C-n>)
-  --       -- the terminal each time. Available in `terminal` mode.
-  --       extra_mappings = {
-  --         enable = true, -- Disable them entirely
-  --         escape = "<C-x>", -- Escape terminal mode
-  --         left = "<c-h>", -- Move to the left window
-  --         down = "<C-j>", -- Move to the window down
-  --         up = "<C-k>", -- Move to the window up
-  --         right = "<C-l>", -- Move to right window
-  --       },
-  --     }
-  --   end,
-  -- },
-
   {
     "lewis6991/gitsigns.nvim",
     dependencies = {
@@ -1292,14 +1259,14 @@ require("lazy").setup {
         desc = "References",
       },
       {
-        "gi",
+        "\\gi",
         function()
           Snacks.picker.lsp_implementations()
         end,
         desc = "Goto Implementation",
       },
       {
-        "gy",
+        "\\gy",
         function()
           Snacks.picker.lsp_type_definitions()
         end,
