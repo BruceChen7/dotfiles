1b961e036f496f4506aba2093c50a60341596213
bofengqiye <bofengqiye7@gmail.com>
2025-09-04 15:30:23 +0800

    chore: update configurations and editor settings
    - Update .gitignore to exclude .pid files
    - Configure Claude Code router with new provider settings
    - Update Neovim plugins configuration with Claude Code integration
    - Adjust Zed editor default model settings

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -757,28 +757,28 @@ require("lazy").setup {
       require "config/mcphub"
     end,
   },
-  {
-    "yetone/avante.nvim",
-    lazy = false,
-    build = "make BUILD_FROM_SOURCE=true",
-    -- version = "v0.*",
-    dependencies = {
-      -- "nvim-tree/nvim-web-devicons",
-      "echasnovski/mini.nvim",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      {
-        "grapp-dev/nui-components.nvim",
-        dependencies = {
-          "MunifTanjim/nui.nvim",
-        },
-      },
-    },
-    config = function()
-      require "config/avante"
-    end,
-  },
-
+  -- {
+  --   "yetone/avante.nvim",
+  --   lazy = false,
+  --   build = "make BUILD_FROM_SOURCE=true",
+  --   -- version = "v0.*",
+  --   dependencies = {
+  --     -- "nvim-tree/nvim-web-devicons",
+  --     "echasnovski/mini.nvim",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     {
+  --       "grapp-dev/nui-components.nvim",
+  --       dependencies = {
+  --         "MunifTanjim/nui.nvim",
+  --       },
+  --     },
+  --   },
+  --   config = function()
+  --     require "config/avante"
+  --   end,
+  -- },
+  --
   -- "Preview command results with `:Norm`"
   {
     "smjonas/live-command.nvim",
@@ -1001,6 +1001,35 @@ require("lazy").setup {
     end,
     keys = {
       { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+    },
+  },
+
+  {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    opts = {
+      terminal_cmd = "ccr code", -- Point to local installation
+    },
+    config = true,
+    keys = {
+      -- Your keymaps here
+      -- { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil", "minifiles" },
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
   },
   {
